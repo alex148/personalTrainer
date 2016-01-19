6d99ddcbd88c924d606c2fa51268a4aa86e71f98
Alexandre Brosse <alexandre.brosse148@gmail.com>
2016-01-19 11:35:35 +0100

    add first servlet

---
+++ src/servlet/FormServlet.java
@@ -0,0 +1,18 @@
+package servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Alex on 19/01/2016.
+ */
+public class FormServlet extends HttpServlet {
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+}
