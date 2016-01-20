eaf7de367c7227c00ebfe902040567db4ddc084c
Thomas <thomashenry@hotmail.fr>
2016-01-20 09:05:44 +0100

    splashscreen + librairies WEBINF

--- src/servlet/FormServlet.java
+++ src/servlet/FormServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import com.google.appengine.api.datastore.DatastoreService;
 /**
  * Created by Alex on 19/01/2016.
  */
@@ -14,7 +14,6 @@ public class FormServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
 
     }
 }
